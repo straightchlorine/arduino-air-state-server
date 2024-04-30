91d48f0a3b6ff5eb9785de4b79dc901ee436604c
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2024-04-30 13:38:41 +0200

    nodemcu: fully functional, bare version
    OLED displays the temperature, pressure, CO2 concentration as well as
    network  and address of the node. The rest of the measurements is packed
    into JSON and sent via /circumstances handle.

--- nodemcu/nodemcu.ino
+++ nodemcu/nodemcu.ino
@@ -10,32 +10,27 @@
 #include <MQUnifiedsensor.h>
 #include <Adafruit_BMP085.h>
 
+// definitions for MQ-135 gas sensor
 #define BOARD              ("ESP8266")
 #define PIN                (A0)
 #define TYPE               ("MQ-135")
 #define VOLTAGE_RESOLUTION (3.3)
 #define ADC_BIT_RESOLUTION (10)
-#define RATIOMQ135CLEANAIR (3.6) 
+#define RATIOMQ135CLEANAIR (3.6)
 
-#define NETSSID ("wifi-ssid")
-#define NETPASS ("wifi-password")
-
+// header contains ssid and password to the wireless network
 #include "secret.h"
 
 Adafruit_SSD1306 display(128, 64, &Wire);
 MQUnifiedsensor MQ135(BOARD, VOLTAGE_RESOLUTION, ADC_BIT_RESOLUTION, PIN, TYPE);
 Adafruit_BMP085 bmp;
 
-// wifi setup
+// wifi network setup
+AsyncWebServer server(80);
 const char *ssid = NETSSID;
 const char *password = NETPASS;
 
-AsyncWebServer server(80);
-
-IPAddress localIP(192, 168, 1, 107);
-IPAddress gateway(192, 168, 1, 1);
-IPAddress subnet(255, 255, 255, 0);
-
+// MQ-135 gas sensor variables
 String ppmCO;
 String ppmCO2;
 String ppmAlcohol;
@@ -43,134 +38,205 @@ String ppmNH4;
 String ppmAceton;
 String ppmToulen;
 
+// BMP180 temperature and pressure sensor variables
 String temperature;
 String pressure;
 String seaLevelPressure;
 String altitude;
 
-// Timer variables
-unsigned long previousMillis = 0;
-const long interval = 10000;
+// connection status
+String connectionPrompt;
 
+// TODO: if free add flags which will indicate if the sensor is connected
+// and assign appropriate variables to the screen
 void setup() {
-    // serial connection
+    pinMode(LED_BUILTIN, OUTPUT);
     Serial.begin(9600);
+    Serial.println("<.> Initializing the peripherals:");
 
-    // set up the OLED display
-    Serial.println();
-    Serial.println();
-    Serial.println("<.> Setting up OLED display...");
     initOLED();
-    Serial.println("<-> success.");
-
-    // set up the MQ-135 gas sensor
-    Serial.println("<.> Setting up MQ-135 gas sensor...");
     initMQ135();
-    Serial.println("<-> success.");
-
-    // set up BMP180 sensor
-    Serial.println("<.> Setting up BMP180 temperature and pressure sensor...");
     initBMP180();
-    Serial.println("<-> success.");
 
-    // set up ESP8266 as an access point
-    Serial.println("<.> Setting up the wireless connection...");
-    initAP();
-
-    // set up the web server
-    Serial.println("<.> Setting up the web server...");
-    initWebServer();
-    Serial.println("<-> Setting up finished. HTTP server is now online!");
+    // web server starts only if the connection is established
+    if (initConnection()) {
+        initWebServer();
+    }
 }
 
-void initOLED() {
-    display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-    display.setTextSize(1);
-    display.setTextColor(WHITE);
+void loop() {
+    readBMP180();
+    readMQ135();
+    displayData();
+    delay(500);
+}
+
+void readBMP180() {
+    // Read pressure and temperature directly into variables
+    pressure = bmp.readPressure() / 100.0F;
+    temperature = bmp.readTemperature();
+
+    // Save the pressure at the sea level and convert to hPa
+    float seaLevelPressurePascals = bmp.readSealevelPressure(150);
+    seaLevelPressure = seaLevelPressurePascals / 100.0F;
+
+    // Calculate altitude using previously read sea level pressure
+    altitude = bmp.readAltitude(seaLevelPressurePascals);
+}
+
+void readMQ135() {
+    MQ135.update();
+
+    MQ135.setA(605.18); MQ135.setB(-3.937);
+    ppmCO = MQ135.readSensor();
+
+    MQ135.setA(110.47); MQ135.setB(-2.862);
+    ppmCO2 = MQ135.readSensor() + 400;
+
+    MQ135.setA(102.2 ); MQ135.setB(-2.473);
+    ppmNH4 = MQ135.readSensor();
+
+    MQ135.setA(77.255); MQ135.setB(-3.18);
+    ppmAlcohol = MQ135.readSensor();
+
+    MQ135.setA(44.947); MQ135.setB(-3.445);
+    ppmToulen = MQ135.readSensor();
+
+    MQ135.setA(34.668); MQ135.setB(-3.369);
+    ppmAceton = MQ135.readSensor();
+}
+
+void displayData() {
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.println();
+    display.print(temperature);
+    display.print("*C, ");
+    display.print(pressure);
+    display.println(" hPa");
+    display.print("\nC02: ");
+    display.print(ppmCO2);
+    display.println();
+    display.println();
+    display.println(connectionPrompt);
+    display.display();
+}
+
+bool initOLED() {
+    Serial.println("<.> Setting up OLED display...");
+    if (display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+        display.clearDisplay();
+        display.setTextSize(1);
+        display.setTextColor(WHITE);
+        Serial.println("<-> success.");
+        return true;
+    } else {
+        Serial.println("<!> failure.");
+        return false; 
+    } 
 }
 
 void initMQ135() {
+    Serial.println("<.> Setting up MQ-135 gas sensor...");
     MQ135.setRegressionMethod(1);
-    MQ135.init(); 
-    calibrate();
+    MQ135.init();
+    calibrateMQ135();
+    Serial.println("<-> success.");
 }
 
-void measurePPM() {
+void calibrateMQ135() {
+    float calcR0 = 0;
 
-    MQ135.update(); // Update data, the arduino will read the voltage from the analog pin
+    for(int i = 1; i <= 10; i ++)
+    {
+        MQ135.update();
+        calcR0 += MQ135.calibrate(RATIOMQ135CLEANAIR);
+    }
+    MQ135.setR0(calcR0/10);
 
-    MQ135.setA(605.18); MQ135.setB(-3.937); // Configure the equation to calculate CO concentration value
-    ppmCO = MQ135.readSensor(); // Sensor will read PPM concentration using the model, a and b values set previously or from the setup
-
-    MQ135.setA(110.47); MQ135.setB(-2.862); // Configure the equation to calculate CO2 concentration value
-    ppmCO2 = MQ135.readSensor() + 400; // Sensor will read PPM concentration using the model, a and b values set previously or from the setup
-
-    MQ135.setA(102.2 ); MQ135.setB(-2.473); // Configure the equation to calculate NH4 concentration value
-    ppmNH4 = MQ135.readSensor(); // Sensor will read PPM concentration using the model, a and b values set previously or from the setup
-
-    MQ135.setA(77.255); MQ135.setB(-3.18); //Configure the equation to calculate Alcohol concentration value
-    ppmAlcohol = MQ135.readSensor(); // SSensor will read PPM concentration using the model, a and b values set previously or from the setup
-
-    MQ135.setA(44.947); MQ135.setB(-3.445); // Configure the equation to calculate Toluen concentration value
-    ppmToulen = MQ135.readSensor(); // Sensor will read PPM concentration using the model, a and b values set previously or from the setup
-    
-    MQ135.setA(34.668); MQ135.setB(-3.369); // Configure the equation to calculate Aceton concentration value
-    ppmAceton = MQ135.readSensor(); // Sensor will rea
-}
-
-void initBMP180() {
-    if (!bmp.begin()) {
-        Serial.println("<!> Could not find a valid sensor, check wiring!");
-        while (1) {}
+    if(isinf(calcR0)) {
+        // open circuit
+        Serial.println("<!> failure.");
+    }
+    if(calcR0 == 0) {
+        // analog to ground
+        Serial.println("<!> failure.");
     }
 }
 
-void initAP() {
+bool initBMP180() {
+    Serial.println("<.> Setting up BMP180 temperature and pressure sensor...");
+    if (bmp.begin()) {
+        Serial.println("<-> success.");
+        return true;
+    }
+    else {
+        Serial.println("<!> failure.");
+        return false;
+    }
+}
+
+bool initConnection() {
+    Serial.println("<.> Setting up the wireless connection...");
     WiFi.mode(WIFI_STA);
     WiFi.disconnect();
     WiFi.begin(ssid, password);
+
     Serial.print("<.> Connecting to " + WiFi.SSID() + " WiFi network");
+    unsigned long startTime = millis();
     while (WiFi.status() != WL_CONNECTED) {
+        // timeout after 10 seconds
+        if (millis() - startTime >= 10000) {
+            Serial.println();
+            Serial.println("<!> failure - connection timed out.");
+            connectionPrompt = "\nbrak polaczenia!";
+            return false;
+        }
         Serial.print(".");
         delay(500);
     }
+
+    blink();
     Serial.println();
     Serial.print("<-> Connected to " + WiFi.SSID() + " at address: ");
     Serial.println(WiFi.localIP());
+    connectionPrompt = "\n" + WiFi.SSID() + ": " + WiFi.localIP().toString();
+
+    Serial.println("<-> success.");
+    return true;
 }
 
 void initWebServer() {
+    Serial.println("<.> Setting up the web server...");
     server.on("/circumstances", HTTP_GET, [](AsyncWebServerRequest *request){
         String json_response = "";
         json_response += "{\"nodemcu\":{";
         json_response += "\"mq135\":{";
-        json_response += "\"co\":\"" + readPPM("co") + "\",";
-        json_response += "\"co2\":\"" + readPPM("co2") + "\",";
-        json_response += "\"alcohol\":\"" + readPPM("alcohol") + "\",";
-        json_response += "\"nh4\":\"" + readPPM("nh4") + "\",";
-        json_response += "\"aceton\":\"" + readPPM("aceton") + "\",";
-        json_response += "\"toulen\":\"" + readPPM("toulen") + "\"";
+        json_response += "\"co\":\"" + readMQ135("co") + "\",";
+        json_response += "\"co2\":\"" + readMQ135("co2") + "\",";
+        json_response += "\"alcohol\":\"" + readMQ135("alcohol") + "\",";
+        json_response += "\"nh4\":\"" + readMQ135("nh4") + "\",";
+        json_response += "\"aceton\":\"" + readMQ135("aceton") + "\",";
+        json_response += "\"toulen\":\"" + readMQ135("toulen") + "\"";
         json_response += "},";
         json_response += "\"bmp180\":{";
-        json_response += "\"temperature\":\"" + readTemperature() + "\",";
-        json_response += "\"pressure\":\"" + readPressure() + "\",";
-        json_response += "\"seaLevelPressure\":\"" + readSeaLevelPressure() + "\",";
-        json_response += "\"altitude\":\"" + readAltitude() + "\"";
+        json_response += "\"temperature\":\"" + readBMP180("temperature") + "\",";
+        json_response += "\"pressure\":\"" + readBMP180("pressure") + "\",";
+        json_response += "\"seaLevelPressure\":\"" + readBMP180("sealevelpressure") + "\",";
+        json_response += "\"altitude\":\"" + readBMP180("altitude") + "\"";
         json_response += "}";
         json_response += "}";
         json_response += "}";
 
         request->send(200, "application/json", json_response);
-        Serial.println("<^> GET /circumstance");
     });
 
-    // Start server
     server.begin();
-    Serial.println("<.> HTTP server started.");
+    Serial.println("<-> Setting up finished. HTTP server is now online!");
+    blink();
 }
 
-String readPPM(String molecule) {
-
+String readMQ135(String molecule) {
     if (molecule == "co")
         return ppmCO;
     else if (molecule == "co2")
@@ -186,78 +252,26 @@ String readPPM(String molecule) {
     return "no data";
 }
 
-/*
-    Read BMP180 sensor and returns the temperature value.
-*/
-String readTemperature() {
-    return temperature;
+String readBMP180(String param) {
+    if (param == "temperature")
+        return temperature;
+    else if (param == "pressure")
+        return pressure;
+    else if (param == "altitude")
+        return altitude;
+    else if (param == "sealevelpressure")
+        return seaLevelPressure;
+    return "no data";
 }
 
-/*
-    Read BMP180 sensor and returns the pressure value.
-*/
-String readPressure() {
-    return pressure;
-}
-
-String readSeaLevelPressure() {
-    return seaLevelPressure;
-}
-
-/*
-    Read BMP180 sensor and returns the altitude value
-    for standard sea level pressure 1013.25hPa.
-*/
-String readAltitude() {
-    return altitude;
-}
-
-/*
-    Calibrates the MQ-135 sensor. It will calculate the R0 value and store it in the object.
-*/
-void calibrate() {
-    float calcR0 = 0;
-
-    for(int i = 1; i <= 10; i ++)
-    {
-        MQ135.update();
-        calcR0 += MQ135.calibrate(RATIOMQ135CLEANAIR);
+/* Generates three quick blinks of the builtin led */
+void blink() {
+    for (int i = 0; i < 3; i++) {
+        digitalWrite(LED_BUILTIN, LOW);
+        delay(250);
+        digitalWrite(LED_BUILTIN, HIGH);
+        delay(250);
     }
-    MQ135.setR0(calcR0/10);
-    
-    if(isinf(calcR0)) {
-        Serial.println("Warning: Conection issue, R0 is infinite (Open circuit detected) please check your wiring and supply"); 
-        while(1);
-    }
-    if(calcR0 == 0) {
-        Serial.println("Warning: Conection issue found, R0 is zero (Analog pin shorts to ground) please check your wiring and supply");
-        while(1);
-    }
-}
-
-void loop() {
-    measurePPM(); 
-    
-    temperature = bmp.readTemperature();
-    pressure = bmp.readPressure() / 100;
-    float slp = bmp.readSealevelPressure(150);
-    seaLevelPressure = slp / 100.0F;
-    altitude = bmp.readAltitude(slp);
-
-    display.clearDisplay();
-    display.setCursor(0, 0);
-    display.print("\nCO2: ");
-    display.print(ppmCO2);
-    display.print(" ppm");
-    display.print("\n\nTemperatura: ");
-    display.print(temperature);
-    display.print(" *C");
-    display.print("\n\nCisinienie: ");
-    display.print(pressure);
-    display.print(" hPa");
-    display.display();
-
-    delay(500); //Sampling frequency
 }
 
 // vim: filetype=arduino:expandtab:shiftwidth=4:tabstop=4:softtabstop=2:textwidth=80:expandtab
